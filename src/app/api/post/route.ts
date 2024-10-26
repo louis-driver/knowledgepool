fca5bda1f136ac9d5b9af546139c787b8ca686fb
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-10-26 00:01:00 -0500

    Update to return post_id

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -4,7 +4,7 @@ import { handleRouteGET } from '../util/handleRouteGET';
 // Define and export the GET handler function
 export async function GET(request: NextRequest) {
     // Create query to fetch post title and summary data for card display
-    let get_post_query = "SELECT user.username, post.title, post.summary FROM user INNER JOIN post ON user.user_id=post.user_id";
+    let get_post_query = "SELECT post.post_id, user.username, post.title, post.summary FROM user INNER JOIN post ON user.user_id=post.user_id";
     // Return results as a JSON object
     return await handleRouteGET(request, [], get_post_query);
 }
