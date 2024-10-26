51a79272db747512948d5787e2d1def0679e259b
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-10-26 00:03:40 -0500

    Create route to follow a user
    Not required for functionality. To be added later

---
+++ src/app/api/post/user/[user_id]/follow/route.ts
@@ -0,0 +1,17 @@
+/* Resource: https://www.wisp.blog/blog/nextjs-14-app-router-get-and-post-examples-with-typescript */
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+    console.log("Begin POST to follow");
+    const body = await request.json();
+    console.log("POST body:", body);
+
+    // TODO validate and process submissions
+    
+    const response = {
+        message: "You have successfully followed",
+        received: body
+    }
+    return NextResponse.json(response);
+}
