5e20484ffaf9582eb9c6d809d42e688bc30d9a88
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-10-26 00:00:21 -0500

    Create route to get posts with a given category_id_id

---
+++ src/app/api/post/category/[category_id]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { handleRouteGET } from '../../../util/handleRouteGET';
+
+// Define and export the GET handler function
+export async function GET(request: NextRequest, {params}: {params: Promise<{category_id: string}>}) {
+
+    // Get category to filter by from url parameter
+    const category_id = (await params).category_id;
+
+    // Query to fetch post title and summary data for card display
+    let get_post_query = "SELECT category.category_id, category.name, user.username, post.title, post.summary FROM user INNER JOIN post ON user.user_id=post.user_id INNER JOIN post_category_bridge ON post.post_id=post_category_bridge.post_id INNER JOIN category ON category.category_id=post_category_bridge.category_id WHERE category.category_id=?";
+ 
+    // Array to pass parameters to SQL query
+    let values: any[] = [category_id];
+
+    // Return NextResponse of query values in json format
+    return await handleRouteGET(request, values, get_post_query);
+}
