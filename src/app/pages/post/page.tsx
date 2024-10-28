91a29e6104bb50096f7cdc1cef35b4d316482db9
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-10-28 10:22:02 -0500

    Move page for dsiplaying list of posts

--- src/app/pages/post/page.tsx
+++ src/app/pages/post/page.tsx
@@ -3,25 +3,31 @@
 import { notFound } from "next/navigation"
 
 interface Post {
+    post_id: number
     username: string
     title: string
     summary: string
 }
 
+/* According to https://vercel.com/blog/common-mistakes-with-the-next-js-app-router-and-how-to-fix-them 
+ it may be better to put my GET requests within the pages rather than
+ fetching from an api folder, so that may be worth researching 
+ and refactoring later
+ */
 async function getPosts() {
-    let res = await fetch(`http://localhost:3000/api/post/all`);
+    let res = await fetch(`http://localhost:3000/api/post/`);
     let posts: Post[] = await res.json();
     if (!posts) notFound();
     return posts;
 }
 
 export async function generateStaticParams() {
-    let posts = await fetch('http://localhost:3000/api/post/all').then((res) =>
+    let posts = await fetch('http://localhost:3000/api/post/').then((res) =>
         res.json()
     );
 
     return posts.map((post: Post) => ({
-        id: post.username,
+        id: post.post_id,
     }));
 }
 
@@ -41,7 +47,7 @@ export default async function Page() {
     return (
         <main>
             {posts.map((post: Post) =>
-                <article key={post.username}>
+                <article key={post.post_id}>
                     <h1>{post.title}</h1>
                     <h2>{post.username}</h2>
                     <p>{post.summary}</p>
