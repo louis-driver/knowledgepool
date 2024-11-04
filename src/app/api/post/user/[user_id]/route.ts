74d1b9670704cb3a63d86c755b031f45272a559d
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-04 15:14:22 -0600

    Update path and keys

--- src/app/api/post/user/[user_id]/route.ts
+++ src/app/api/post/user/[user_id]/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest, {params}: {params: Promise<{user
     const user_id = (await params).user_id;
 
     // Create query to fetch post title and summary data for card display
-    let get_post_query = "SELECT user.username, post.title, post.summary FROM user INNER JOIN post ON user.user_id=post.user_id WHERE user.user_id=?";
+    let get_post_query = "SELECT post.post_id, user.username, post.title, post.summary FROM user INNER JOIN post ON user.user_id=post.user_id WHERE user.user_id=?";
 
     // Array to pass parameters to SQL query
     let values: any[] = [user_id];
--- src/app/post/user/[user_id]/page.tsx
+++ src/app/post/user/[user_id]/page.tsx
@@ -3,6 +3,7 @@
 import { notFound } from "next/navigation"
 
 interface Post {
+    post_id: string
     username: string
     title: string
     summary: string
@@ -10,7 +11,7 @@ interface Post {
 
 async function getPosts(user_id: string) {
     console.log("Fetching posts by user", {user_id});
-    let res = await fetch(`http://localhost:3000/api/post/${user_id}`);
+    let res = await fetch(`http://localhost:3000/api/post/user/${user_id}`);
     let posts: Post[] = await res.json();
     if (!posts) notFound();
     return posts;
@@ -26,15 +27,12 @@ export async function generateMetadata(user_id: string) {
 
 export default async function Post({params}: {params: Promise<{user_id: string}>}) {
     const {user_id} = await params;
-    console.log("Component received Category ID:", {user_id});
     const posts = await getPosts(user_id);
-
-    console.log(posts);
 
     return (
         <main>
             {posts.map((post: Post) =>
-                <article key={post.username}>
+                <article key={post.post_id}>
                     <h1>{post.title}</h1>
                     <h2>{post.username}</h2>
                     <p>{post.summary}</p>
