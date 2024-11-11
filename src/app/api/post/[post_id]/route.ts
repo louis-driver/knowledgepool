0aef56ea0afefe5365180f769fa61caf461ef891
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-11 10:34:13 -0600

    Update query to get necessary post data

--- src/app/api/post/[post_id]/route.ts
+++ src/app/api/post/[post_id]/route.ts
@@ -4,11 +4,11 @@ import { handleRouteGET } from '../../util/handleRouteGET';
 // Define and export the GET handler function
 export async function GET(request: NextRequest, {params}: {params: Promise<{post_id: string}>}) {
 
-    // Get category to filter by from url parameter
+    // Get post_id  from url parameter
     const post_id = (await params).post_id;
 
     // Query to fetch post information to create a page
-    let get_post_query = "SELECT post.post_id, user.user_id, user.username, post.title, post.summary, post.content FROM user INNER JOIN post ON user.user_id=post.user_id WHERE post.post_id=?";
+    let get_post_query = "SELECT post.post_id, user.username, post.response_to, post.title, post.summary, post.content, post.create_time FROM user INNER JOIN post ON user.user_id=post.user_id WHERE post.post_id=?";
 
     // Array to pass parameters to SQL query
     let values: any[] = [post_id];
