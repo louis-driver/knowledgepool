3414cd1c53004c9cc9d42e8a88d75d70992b480f
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-11 10:16:49 -0600

    Create barebones footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h1>Contact</h1>
+            <ul>
+                <li>emailgoeshere@email.com</li>
+                <li>1-800-111-1111</li>
+            </ul>
+        </footer>
+    )
+}
