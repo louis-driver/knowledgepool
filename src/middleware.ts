0f906a2bad64f951701ab7074772b89b3a279af7
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-11 10:18:32 -0600

    Remove unnecessary logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { decrypt, getSession } from "@/app/lib/session";
-import { cookies } from 'next/headers';
-
-// Protected routes
-const protectedRoutes = ['/post', '/review'];
-const publicRoutes = ['/', '/user/signin', '/user/submit'];
+import { getSession } from "@/app/lib/session";
 
 export default async function middleware(req: NextRequest) {
-    // Check if route is protected
-    const path = req.nextUrl.pathname;
-    const isProtectedRoute = protectedRoutes.includes(path);
-    const isPublicRoute = publicRoutes.includes(path);
-    console.log("middleware called for path:", path);
-
-
     // Decrypt session from the cookie
     const session = await getSession();
     console.log("Session:", session)
 
     // Redirect to login if the user is not authenticated 
-    if (session == null) {
+    if (!session) {
         return NextResponse.redirect(new URL('/user/signin', req.nextUrl));
-    }
-
-    // Redirect to /post if user is authenticated
-    if (
-        isPublicRoute &&
-        session?.userId &&
-        !req.nextUrl.pathname.startsWith('/post')
-    ) {
-        return NextResponse.redirect(new URL('post'))
     }
 
     return NextResponse.next();
