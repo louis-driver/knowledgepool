7268382685ed040354dd4915ad3e37aa255da3f6
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-23 19:32:20 -0600

    Add styles and post content for context

--- src/app/post/reviews/[post_id]/page.tsx
+++ src/app/post/reviews/[post_id]/page.tsx
@@ -1,24 +1,30 @@
-import { getReviewsForPost } from "@/app/actions/post";
+import { getPost, getReviewsForPost } from "@/app/actions/post";
+import KnowledgeDrop from "@/components/KnowledgeDrop";
 import { Review } from "@/types/review";
+import "./styles.css";
 
 export default async function Pages({params}: {params: Promise<{post_id: number}>}) {
     const {post_id} = await params;
+    const post = await getPost(post_id);
     const reviews: Review[] = await getReviewsForPost(post_id);
     console.log("Reviews:", reviews);
 
     return (
         <main>
+            <h1 className="page-title">Reviews</h1>
             {/* Conditionally render message if post has no reviews */
             reviews.length == 0 &&
-                <p>Looks like your Drop is still waiting to be filtered!</p>
+                <p className="page-paragraph">Looks like your Drop is still waiting to be filtered!</p>
             }
             {/* Create a card for each review */
             reviews.map((review: Review) => 
-                <section key={review.review_id}>
-                    <h2>Approval: <span>{review.approval_rating}</span></h2>
-                    <p>{review.comments}</p>
+                <section key={review.review_id} className="knowledge-drop-card">
+                    <h2 className="review-card-title">Rating: <b>{review.approval_rating}</b></h2>
+                    <p className="review-card-comments">{review.comments}</p>
                 </section>
             )}
+            <h1 className="page-title">Drop Reviewed</h1>
+            <KnowledgeDrop post={post} />
         </main>
     )
 }
