edf009d455d8c637ef34a20369a38246af2db181
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-23 19:28:29 -0600

    Update styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,15 @@ import "./styles/Navbar.css"
 export default function Navbar() {
     return (
         <nav>
-            <h1 className="logo">KnowledgePool</h1>
+            <div className="logo">
+                <div className="logo-image" />
+                <h1 className="logo-text">KnowledgePool</h1>
+            </div>
             <ul className="link-list">
-                <li><Link href={`/post`}>Knowledge Drops</Link></li>
-                <li><Link href={`/review`}>Filter Drops</Link></li>
+                <li><Link href={`/post`}>The Pool</Link></li>
+                <li><Link href={`/review`}>Filtration System</Link></li>
                 <li><Link href={`/post/reviews`}>My Drops</Link></li>
+                <li><Link href={`/post/submit`}>Create A Drop</Link></li>
             </ul>
         </nav>
     )
