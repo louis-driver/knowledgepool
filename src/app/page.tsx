4ba5ec714ce2f9e63c66bbac7391c37688ad872f
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-11-27 18:07:10 -0600

    Update paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
             <h1 className="page-title">Welcome to KnowledgePool!</h1>
             <p className="page-paragraph">The time is gone that social media is designed to feed off an endless cycle of users addictively consuming content. At KnowledgePool we want you to come for a swim, and then be on your way. Refreshed and renewed, ready to apply what you’ve learned to your actual life!</p>
             <p>
-                <Link href={"/user/signup"} className="form-link">Sign Up Here!</Link>
+                <Link href={"/user/submit"} className="form-link">Sign Up Here!</Link>
                 <span> or </span>
-                <Link href={"/user/submit"} className="form-link">Log In Here</Link>
+                <Link href={"/user/signin"} className="form-link">Log In Here</Link>
             </p>
         </main>
     );
