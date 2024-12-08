15a37f8cb7f5a0b0c1be0841e34c9f9c84c70cd0
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-12-07 19:43:52 -0600

    Create test for NavBar component

---
+++ __test__/components/Navbar.test.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Navbar from '../../src/components/Navbar'
+ 
+describe('Navbar', () => {
+  it('renders KnowledgePool heading', () => {
+    render(<Navbar />)
+ 
+    const heading = screen.getByRole('heading', { level: 1 })
+ 
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveTextContent("KnowledgePool")
+  })
+
+  it('renders navigation links', () => {
+    render(<Navbar />)
+
+    const linkToPosts = screen.getByRole('link', { name: "The Pool" })
+    expect(linkToPosts.getAttribute("href")).toBe("/post")
+    
+    const linkToReviewForm = screen.getByRole('link', { name: "Filtration System" })
+    expect(linkToReviewForm.getAttribute("href")).toBe("/review")
+    
+    const linkToUserSubmissions = screen.getByRole('link', { name: "My Drops" })
+    expect(linkToUserSubmissions.getAttribute("href")).toBe("/post/reviews")
+    
+    const linkToPostCreation = screen.getByRole('link', { name: "Create A Drop" })
+    expect(linkToPostCreation.getAttribute("href")).toBe("/post/submit")
+  })
+})
