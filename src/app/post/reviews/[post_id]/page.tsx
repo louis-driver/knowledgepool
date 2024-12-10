42c06a5822ff6e2c4938b93e588590bef65e1a2b
louis-driver <90572825+louis-driver@users.noreply.github.com>
2024-12-09 18:39:34 -0600

    Add checks to display received errors

--- src/app/post/reviews/[post_id]/page.tsx
+++ src/app/post/reviews/[post_id]/page.tsx
@@ -1,32 +1,53 @@
 import { getPost, getReviewsForPost } from "@/app/actions/post";
 import KnowledgeDrop from "@/components/KnowledgeDrop";
-import { Review } from "@/types/review";
+import { isReviewArray, Review } from "@/types/review";
 import "./styles.css";
+import { isPostForDisplay } from "@/types/post";
 
 export default async function Pages({params}: {params: Promise<{post_id: number}>}) {
     const {post_id} = await params;
     const post = await getPost(post_id);
-    const reviews: Review[] = await getReviewsForPost(post_id);
-    console.log("Reviews:", reviews);
+    const reviews = await getReviewsForPost(post_id);
 
-    return (
-        <main>
-            <h1 className="page-title">Reviews</h1>
-            {/* Conditionally render message if post has no reviews */
-            reviews.length == 0 &&
-                <p className="page-paragraph">Looks like your Drop is still waiting to be filtered!</p>
-            }
-            {/* Create a card for each review */
-            reviews.map((review: Review) => 
-                <section key={review.review_id} className="knowledge-drop-card">
-                    <h2 className="review-card-title">Rating: <b>{review.approval_rating}</b></h2>
-                    <p className="review-card-comments">{review.comments}</p>
-                </section>
-            )}
-            <h1 className="page-title">Drop Reviewed</h1>
-            <KnowledgeDrop post={post} />
-        </main>
-    )
+    if (isPostForDisplay(post)) {
+        if (isReviewArray(reviews)) {
+            return (
+                <main>
+                    <h1 className="page-title">Reviews</h1>
+                    {/* Conditionally render message if post has no reviews */
+                    reviews.length == 0 &&
+                        <p className="page-paragraph">Looks like your Drop is still waiting to be filtered!</p>
+                    }
+                    {/* Create a card for each review */
+                    reviews.map((review: Review) => 
+                        <section key={review.review_id} className="knowledge-drop-card">
+                            <h2 className="review-card-title">Rating: <b>{review.approval_rating}</b></h2>
+                            <p className="review-card-comments">{review.comments}</p>
+                        </section>
+                    )}
+                    <h1 className="page-title">Drop Reviewed</h1>
+                    <KnowledgeDrop post={post} />
+                </main>
+            )
+         }
+         else {
+            return (
+                <main>
+                    <p className="page-paragraph">Error occurred while fetching reviews for this post.</p>
+                    <h2 className="page-title">Status: {reviews.returnedStatus}</h2>
+                    <p className="page-paragraph">{reviews.error}</p>
+                </main>
+            )
+         }
+    }
+    else {
+        return (
+            <main>
+                <h2 className="page-title">Status: {post.returnedStatus}</h2>
+                <p className="page-paragraph">{post.error}</p>
+            </main>
+        )
+    }
 }
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/page 
